ff10d14cfee492f0afef9b7a2a303d25ef40e82c
VEERI JAY DURGA SURENDRA GOWDA <163961716+surendraveeri@users.noreply.github.com>
2025-03-27 11:57:13 +0530

    Add files via upload

--- constants.js
+++ constants.js
@@ -1,1 +1,10 @@
-export const API_URL = "https://expense-backend-rs.herokuapp.com/";
+'use strict';
+
+module.exports = {
+  BINARY_TYPES: ['nodebuffer', 'arraybuffer', 'fragments'],
+  GUID: '258EAFA5-E914-47DA-95CA-C5AB0DC85B11',
+  kStatusCode: Symbol('status-code'),
+  kWebSocket: Symbol('websocket'),
+  EMPTY_BUFFER: Buffer.alloc(0),
+  NOOP: () => {}
+};
